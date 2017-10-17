c07e4bb0124286c61b8ab61d01871488fa4b913a
mollie chacon <mollie.mosley@gmail.com>
2017-10-16 18:36:35 -0600

    initial files and starter db code

---
+++ bamazonseeds.sql
@@ -0,0 +1,8 @@
+USE bamazon_db;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Water Bottle", "Housewares", 10, 8), ("Potato Chips","Dry Food Goods", 2, 26), 
+("LCD Monitor", "Computers and Hardware", 100, 8), ("Trek Bicycle", "Sporting Goods", 700, 3),
+("Calculator", "Office Supplies", 4, 27), ("Coca Cola 6-Pack", "Dry Food Goods", 3, 35),
+("Fuji Apple", "Fresh Food", 1, 150), ("Screwdriver Set", "Hardware", 11, 6),
+("Paper Shredder", "Office Supplies", 65, 4), ("Wall Calendar", "Office Supplies", 4, 17);
---
+++ schema.sql
@@ -0,0 +1,12 @@
+-- DROP DATABASE IF EXISTS bamazon_db;
+CREATE database bamazon_db;
+USE bamazon_db;
+
+CREATE TABLE products (
+item_id INT AUTO_INCREMENT NOT NULL,
+product_name VARCHAR(50),
+department_name VARCHAR(50),
+price INT,
+stock_quantity INT,
+PRIMARY KEY(item_id)
+);
